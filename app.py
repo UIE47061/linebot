3901e0aaf07077a8ba8f4a01a1c064ecfd5c1f5c
UIE47061 <114350701+UIE47061@users.noreply.github.com>
2022-09-26 00:21:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,101 @@
-import os
-from datetime import datetime
-
-from flask import Flask, abort, request
-
-# https://github.com/line/line-bot-sdk-python
+from __future__ import unicode_literals
+from operator import index
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import configparser
+import random
+
+cho = False #抽
+fn = False  #分
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 
-@app.route("/", methods=["GET", "POST"])
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
 def callback():
+    signature = request.headers['X-Line-Signature']
 
-    if request.method == "GET":
-        return "Hello Heroku"
-    if request.method == "POST":
-        signature = request.headers["X-Line-Signature"]
-        body = request.get_data(as_text=True)
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)    
+    try:
+        #print(body, signature)
+        handler.handle(body, signature)        
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-
-        return "OK"
-
-
+# 學你說話
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
+def pretty_echo(event):
+    global cho, fn
+    def Input():
+        return event.message.text
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    def chose():
+        A,B,N = map(int,Input().split())
+        lst = []
+        for i in range(A, B+1):
+            lst.append(i)
+        print(lst)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(random.choices(lst, k=N))))
+
+    def team():
+        lst = list(map(int,Input().split()))
+        person = []
+        for i in range(1, lst[0] + 1):
+            person.append(i)
+        del lst[0]
+        G = lst.pop(0)
+        for i in lst:
+            if(i in person):
+                person.remove(i)
+        while(G not in person): #怕分界沒來
+            G += 1
+        p = person.index(G)
+        Blst,Glst = person[:p],person[p:]
+        random.shuffle(Blst)
+        random.shuffle(Glst)
+        Bp = len(Blst)// 2 + 1
+        Gp = len(Glst)// 2 - 1 
+        team1 = sorted(Blst[:Bp] + Glst[:Gp])
+        team2 = sorted(Blst[Bp:] + Glst[Gp:])
+        output = 'team1=>' + str(team1) + '\n' + 'team2=>' + str(team2)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=output))
+
+        
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":        
+        if(cho): #抽
+            chose()
+            cho = False
+        elif(fn): #分
+            team()
+            fn = False
+        
+        if(Input() == '抽'):
+            cho = True
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='幾到幾,幾個(X X X)'))
+            return     
+        elif(Input() == '分'):
+            fn = True
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='全總共幾人,女生第一號,沒來的座號(X X X)'))
+            return
+        # Phoebe 愛唱歌
+        # pretty_note = '♫♪♬'
+        # pretty_text = ''
+        # for i in event.message.text:
+        #     pretty_text += i
+        #     pretty_text += random.choice(pretty_note)
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=pretty_text))
+
+if __name__ == "__main__":
+    app.run()
